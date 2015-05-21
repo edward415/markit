0aa8c639db380def59eb398be083973cc9af025e
Ed C <edc415@outlook.com>
2015-05-21 09:54:34 +0000

    final touches

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,7 @@
 class Topic < ActiveRecord::Base
   belongs_to :user
   has_many :bookmarks, dependent: :destroy
+  default_scope { order('updated_at DESC') }
   
   def to_param
     "#{id} #{title}".parameterize
